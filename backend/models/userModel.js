2f5aa3cb913d78b4ec251527e59f392e94e627df
Ath1thyan <athitasks@gmail.com>
2024-06-10 07:55:53 +0530

    user registration completed

---
+++ backend/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password length must be between 6 to 32 characters"],
+        trim: true,
+        match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/, 'Password must contain at least 6 characters, including UPPER/lowercase and numbers'],
+        maxlength: [32, "Password must not exceed 32  characters"]
+    },
+    photo: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+        required: [true, "Please add a image"]
+    },
+    phone: {
+        type: String,
+        // required: [true, "Phone number is required"]
+        default: "+91",
+    },
+    role: {
+        type: String,
+        maxlength: [64, "Must not exceed 64 characters"],
+        default: "role"
+    }
+},  {
+    timestamps: true,
+})
+
+const User = mongoose.model("User", userSchema)
+module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,31 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/userRoute')
+const errorHandler = require('./middleware/errorMiddleware')
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Routes Middleware
+app.use("/api/users", userRoutes)
+
+
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home");
+})
+
+
+// Error Middleware
+app.use(errorHandler)
 
 // connect to DB & start server
 mongoose
